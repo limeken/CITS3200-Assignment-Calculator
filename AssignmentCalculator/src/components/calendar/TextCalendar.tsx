87c4fce4166c546a624582c6992e3826e5e7019f
Jacob Bennett <jacobbennett1603@gmail.com>
2025-10-02 20:29:44 +0800

    Made the dotpoint drop downs for each assignment step, with a state system that only keeps the selected step open. Changed some formating of elements and made the unit code buttons the same colour as their allocated colour. Need to still add additional resources and an option for filtering by due date

--- AssignmentCalculator/src/components/calendar/TextCalendar.tsx
+++ AssignmentCalculator/src/components/calendar/TextCalendar.tsx
@@ -1,23 +1,46 @@
-import React, { useState, Fragment} from "react";
+import React, { useState, useEffect } from "react";
 import type { AssignmentCalendar } from "./CalendarTypes.ts";
 import { Tab, TabGroup, TabList, TabPanel, TabPanels } from '@headlessui/react'
-import { CheckIcon, ChevronDownIcon } from "@heroicons/react/24/solid";
+import { CheckIcon, ChevronDownIcon, MinusIcon} from "@heroicons/react/24/solid";
 import { assignmentTypes } from "../testdata.ts";
-import type { Assignment } from "./CalendarTypes.ts";
-
 
 // This component displays the steps for valid assignments, in top-down order
-const AssignmentStepsComponent: React.FC<{assignmentTypes:Record<string,Assignment>, assignment:AssignmentCalendar|null}> = ({assignmentTypes, assignment}) => {
+const AssignmentStepsComponent: React.FC<{assignment:AssignmentCalendar|null}> = ({assignment}) => {
+    // Keeps track of what step is currently open
+    const [openStep, setOpenStep] = useState<number>(0);
+
+    // Reset the step count every time a new assignment is shown
+    useEffect(() => {setOpenStep(0);}, [assignment]);
+    
+    // This maps the selected assignment with its assignment type
     const assignmentType = assignment ? assignmentTypes[assignment.assignmentType] : null;
     if(assignment != null){
         return (
-            <div className={`flex flex-col items-center bg-slate-200 rounded-xl shadow-soft p-4 w-4/5`}>
+            <div className={`flex flex-col items-center bg-slate-200 rounded-xl shadow-soft p-4 w-full`}>
                 <h1 className="font-bold">{assignment.unitCode} - {assignment.name}</h1>
-                <div className = "flex flex-col gap-4 items-center w-3/4">
+                <div className = "flex flex-col gap-2 items-center w-3/4">
+                {/* This creates a descending sequence of step elements to show*/}
                 {assignmentType!.events.map((step, index)=>
-                    <div className="bg-slate-300 rounded-xl w-full h-10 flex items-center justify-center gap-2">
-                        <span className="font-bold">Step {index+1}: {step.name}</span>
-                        <ChevronDownIcon className="w-5 h-5"/>
+                    <div key={index} className="flex flex-col gap-4 w-full">
+                        {/* Element that when clicked shows the dot points for a given step */}
+                        <button 
+                        className="bg-slate-300 rounded-xl w-full h-10 flex items-center justify-left pl-4 gap-2 relative"
+                        onClick={()=>setOpenStep(index)}
+                        >
+                            <span className="font-bold">Step {index+1}: {step.name}</span>
+                            {index===openStep?<MinusIcon className="w-5 h-5"/>:<ChevronDownIcon className="w-5 h-5"/>}
+                        </button>
+
+                        {/* Panel that is shown for a given step when selected */}
+                        <div className={`bg-white rounded-xl w-full transition-all duration-300 ease-in-out origin-top
+                            ${index === openStep ? "h-50 p-5 overflow-y-scroll" : "h-0 p-0 overflow-hidden"}`}>
+                            <ul className="mt-2 flex list-disc flex-col gap-3 pl-4 text-md">
+                                {/* Lists all the dot points within a given step's panel */}
+                                {step.instructions && step.instructions.map((dotpoint,id)=>
+                                    <li key={id}>{dotpoint}</li>
+                                )}
+                            </ul>
+                        </div>
                     </div>
                 )}
                 </div>
@@ -33,45 +56,53 @@ const AssignmentStepsComponent: React.FC<{assignmentTypes:Record<string,Assignme
 // Main component representing the textual unit format
 const TextCalendar: React.FC<{show:boolean; assignments:Record<string, AssignmentCalendar[]>}> = ({show, assignments}) => {
     const[currentAssignment, setCurrentAssignment] = useState<AssignmentCalendar|null>(null);
-    return(
-        <section className={`flex flex-col items-center gap-5 mx-auto w-full max-w-6xl px-4 sm:px-6 mt-6 ${show?"":"hidden"}`}>
-            {/* Tab group for unit selection */}
-            <TabGroup className="w-full flex flex-col gap-5 justify-center mx-auto w-full">
-                {/* Lists buttons for each unit*/}
-                <TabList className="bg-slate-200 rounded-xl shadow-soft p-4 grid grid-cols-4 gap-4 w-full">
-                    {Object.keys(assignments).map((code)=> 
-                        <Tab key={code}
-                            className="w-full h-12 bg-uwaBlue rounded-lg text-white px-4 flex items-center justify-between data-selected:bg-blue-800"
-                        >
-                            {({ selected }) => (
-                                <>
-                                <span className="font-bold">{code}</span>
-                                <CheckIcon className={`w-5 h-5 ml-2 text-white ${selected ? 'block' : 'hidden'}`} />
-                                </>
-                            )}
-                        </Tab>
-                    )}
-                </TabList>
 
-                {/* Shows assessment buttons based on unit selected */}
-                <TabPanels className="w-full">
-                    {Object.keys(assignments).map((code)=> 
-                        <TabPanel key={code} className="bg-slate-200 rounded-xl shadow-soft p-4 grid grid-cols-4 gap-4 w-full">
-                            {/* Tab group for assessments associated with a unit */}
-                            {assignments[code].map((assignment)=>
-                            <button 
-                                className="w-full h-12 bg-white rounded-lg px-4 flex items-center justify-between"
-                                onClick={()=>setCurrentAssignment(assignment)}
-                                >
-                                {assignment.name}
-                            </button>
-                            )}
-                        </TabPanel>
-                    )}
-                </TabPanels>
-            </TabGroup>
-            <AssignmentStepsComponent assignmentTypes={assignmentTypes} assignment={currentAssignment}/>
-        </section>
-    )
+    // Rendered only when assignments are available
+    if(Object.keys(assignments).length > 0){
+        return(
+            <section className={`flex flex-col items-center gap-5 mx-auto w-full max-w-6xl px-4 sm:px-6 mt-6 ${show?"":"hidden"}`}>
+                {/* Tab group for unit selection */}
+                <TabGroup className="w-full flex flex-col justify-center mx-auto w-full bg-slate-300 rounded-xl overflow-hidden gap-2">
+                    {/* Lists buttons for each unit*/}
+                    <TabList className="bg-uwaBlue rounded-xl rounded-b-none shadow-soft p-4 grid grid-cols-4 gap-4 w-full">
+                        {Object.keys(assignments).map((code)=> 
+                            <Tab key={code}
+                                className={`w-full h-12 rounded-lg text-white px-4 flex items-center justify-between ${"bg-"+assignments[code][0].color+"-200"} ${"data-selected:bg-"+assignments[code][0].color+"-200"}`}
+                            >
+                                {({ selected }) => (
+                                    <>
+                                    <span className="font-bold">{code}</span>
+                                    <CheckIcon className={`w-5 h-5 ml-2 text-white ${selected ? 'block' : 'hidden'}`} />
+                                    </>
+                                )}
+                            </Tab>
+                        )}
+                    </TabList>
+
+                    {/* Shows assessment buttons based on unit selected */}
+                    <TabPanels className="w-full">
+                        {Object.keys(assignments).map((code)=> 
+                            <TabPanel key={code} className="grid grid-cols-4 gap-4 w-full p-4">
+                                {/* Tab group for assessments associated with a unit */}
+                                {assignments[code].map((assignment)=>
+                                <button 
+                                    className="w-full h-12 bg-white rounded-lg px-4 flex items-center justify-between"
+                                    onClick={()=>setCurrentAssignment(assignment)}
+                                    >
+                                    <span>{assignment.name}</span>
+                                    <div className={`${assignment===currentAssignment?"block":"hidden"}`}>
+                                        <CheckIcon className="w-5 h-5 ml-2"/>
+                                    </div>
+                                </button>
+                                )}
+                            </TabPanel>
+                        )}
+                    </TabPanels>
+                </TabGroup>
+                <AssignmentStepsComponent assignment={currentAssignment}/>
+            </section>
+        )
+    }
+    else{return null;}
 }
 export default TextCalendar;
